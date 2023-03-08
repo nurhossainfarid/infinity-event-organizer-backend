355f81d03a04c57cde3b9b0ab964115ece6aef8d
nurhossainfarid <faahsan1516@gmail.com>
2023-03-08 18:26:10 +0600

    change route dir

--- routes/v1/organization.route.js
+++ routes/v1/organization.route.js
@@ -9,7 +9,7 @@ router
     .route('/')
     .post(organizationController.createOrganization)
     .get(organizationController.getAllOrganization);
-router.route('/count').get(organizationController.getCountOrganizer);
+router.route('/count/c').get(organizationController.getCountOrganizer);
 router.route('/email/:email').get(organizationController.findOrganizerByEmail);
 router.route('/name/:name').get(organizationController.findOrganizerByName);
 router
